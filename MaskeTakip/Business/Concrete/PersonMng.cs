dd608b19596a8228780b2f745be35c4da53532d9
Alperen Bozkurt <alperenbozkurt970@gmail.com>
2021-07-08 10:38:20 +0300

    Kimlik Kontrolü Maske Takip
    Proje 1(Staj)

---
+++ MaskeTakip/Business/Concrete/PersonMng.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class PersonMng : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+        public List<Person> GetList()
+        {
+
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            TCKimlikNoDogrulaRequest xx =  new TCKimlikNoDogrulaRequest();
+            xx.Body = new TCKimlikNoDogrulaRequestBody();
+            xx.Body.Ad = person.FirstName;
+            xx.Body.DogumYili = person.DateOfBirthYear;
+            xx.Body.Soyad = person.LastName;
+            xx.Body.TCKimlikNo = person.NationalIdentity;
+            return client.TCKimlikNoDogrulaAsync(xx).Result.Body.TCKimlikNoDogrulaResult ;
+        }
+    }
+}
---
+++ MaskeTakip/WorkingP/Program.cs
@@ -0,0 +1,37 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace WorkingP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Person person1 = new Person();
+            person1.FirstName = "Erhan";
+            person1.LastName = "Can";
+            person1.NationalIdentity = 3153135131;
+            person1.DateOfBirthYear = 1998;
+
+
+            Person person2 = new Person();
+            person2.FirstName = "Alperen";
+            person2.LastName = "Bozkurt";
+            person2.NationalIdentity = 11101097684;
+            person2.DateOfBirthYear = 1998;
+
+            PttMng pttMng = new PttMng(new PersonMng());
+            pttMng.GiveMask(person1);
+            pttMng.GiveMask(person2);
+            pttMng.GiveMask(person2);
+        }
+
+        static void hosgeldin(string isim)
+        {
+            Console.WriteLine("Merhaba" + isim);
+        }
+        
+    }
+}
